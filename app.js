829577b09a46bd7cb1bf8f043f1326a7fd714043
JulianoKosloski <juliano.koslos@gmail.com>
2022-10-04 13:04:56 -0300

    setting up

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 class Boat extends React.Component {
     render() {
+        return("");
 
+        
+    }
+}
+
+class Square extends React.Component {
+    render() {
         return("");
 
         
@@ -9,17 +16,22 @@ class Boat extends React.Component {
 
 class Board extends React.Component {
     render() {
-
         return("");
 
 
     }
 }
-
+// can use this as <Game name="Battleship"/>
 class Game extends React.Component {
-    render() {
+    render() { //returns a description (a React element) of what should be shown, and React takes this description and displays a result
+        return (
+                //this was written in JSX, which simplifies repetitive calls such as React.createElement('h1', ...) and so on
+            <div class="myTitle">
+                <h1>This is a {this.props.name} game</h1>  
+                <h2>Choose your side:</h2>
+            </div>
 
-        return("");
+        );
 
         
     }
